8d1e34e4074da54fdf8fd20a1ba3d32d44c0cb6a
mohammad <m.zandieh1999@gmail.com>
2021-09-23 15:37:09 +0330

    filed Injection

--- app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
+++ app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.hiltdagger2tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //filed Injection
+    @Inject
+    lateinit var someClass: someClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+    }
+}
+
+class  someClass
+@Inject
+constructor(){
+
+    fun doAThing() :String{
+        return "Look i did a thing !"
     }
 }
