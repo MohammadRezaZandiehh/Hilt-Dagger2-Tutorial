3b7855c66eac510f7093981bf0e240829beb9c9c
mohammad <m.zandieh1999@gmail.com>
2021-09-23 15:52:45 +0330

    constructor Injection

--- app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
+++ app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
@@ -10,20 +10,39 @@ class MainActivity : AppCompatActivity() {
 
     //filed Injection
     @Inject
-    lateinit var someClass: someClass
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println(someClass.doAThing())
+        println(someClass.doSomeOtherThings())
+
     }
 }
 
-class  someClass
+class SomeClass
 @Inject
-constructor(){
+constructor(
+    private val someOtherThing: SomeOtherThing
+) {
 
-    fun doAThing() :String{
+    fun doAThing(): String {
         return "Look i did a thing !"
     }
+
+    fun doSomeOtherThings(): String {
+        return someOtherThing.doSomeOtherThings()
+    }
+}
+
+class SomeOtherThing
+@Inject
+constructor() {
+
+    fun doSomeOtherThings(): String {
+        return "Look i did other things !"
+    }
+
 }
