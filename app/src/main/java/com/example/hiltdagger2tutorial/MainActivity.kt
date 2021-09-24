ea2430d98b9c1c293be54cb409a453f0e7c0e43c
mohammad <m.zandieh1999@gmail.com>
2021-09-24 15:22:55 +0330

    solved Problem in Constructor Injection with @Binds that is a bad way !

--- app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
+++ app/src/main/java/com/example/hiltdagger2tutorial/MainActivity.kt
@@ -3,8 +3,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.hiltdagger2tutorial.R
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,38 +23,47 @@ class MainActivity : AppCompatActivity() {
 
         println(someClass.doAThing())
     }
-
 }
 
 class SomeClass
 @Inject
 constructor(
-//    private val someInterface: SomeInterface
-private val gson: Gson
-) {
-    fun doAThing(): String {
-//        return "Look I got: ${someInterface.getAThing()}"
-        return "something"
+    private val someInterfaceImpl: SomeInterface,
+    private val gson: Gson
+){
+    fun doAThing(): String{
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
-
 }
 
-//class SomeDependency
-//@Inject
-//constructor() {
-//    fun getAThing(): String {
-//        return "A Thing"
-//    }
-//}
+class SomeInterfaceImpl
+@Inject
+constructor(): SomeInterface {
+    override fun getAThing() : String{
+        return "A Thing"
+    }
+}
 
+interface SomeInterface{
+    fun getAThing(): String
+}
 
-//class SomeInterfaceImp() : SomeInterface {
-//    override fun doAThing(): String {
-//        return "do A thing"
-//    }
-//
-//}
+// ***REMEMBER***
+// Notice this doesn't compile because dagger doesn't know how to build the Gson object, even though we added it to the module.
+@InstallIn(ApplicationComponent::class)
+@Module
+abstract class MyModule{
 
-//interface SomeInterface {
-//    fun getAThing(): String
-//}
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl: SomeInterfaceImpl
+    ): SomeInterface
+
+    // note this is from the dependency: "com.squareup.retrofit2:converter-gson:2.6.0"
+    @Singleton
+    @Binds
+    abstract fun bindGson(
+        gson: Gson
+    ): Gson
+}
